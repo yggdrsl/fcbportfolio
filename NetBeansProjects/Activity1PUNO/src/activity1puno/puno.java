c1878a2afee609a8539d1e061510d0224b728eea
yggdrsl <yggdrasail14@gmail.com>
2024-05-25 16:00:00 +0800

    Finished fcb porfolio

---
+++ NetBeansProjects/Activity1PUNO/src/activity1puno/puno.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activity1puno;
+
+/**
+ *
+ * @author Michael
+ */
+public class puno {
+    static int tempertature;
+    public static void fullname(){
+        String firstName = "Michael";
+        String lastName = "Puno";
+        System.out.println(firstName);
+        System.out.println(lastName);
+    }
+    
+    public  static void temperature (){
+         int c = 43;
+         System.out.println(c);
+           
+    }
+   
+    
+}
---
+++ NetBeansProjects/activity1/src/puno.java
@@ -0,0 +1,34 @@
+
+public class puno {
+    static int a = 43;
+        String bak ;
+    
+    public static void fullName(){
+        puno var = new puno();
+        var.bak = "Age";
+    
+    
+            
+           String firstName= "Michael";
+           String name = "First Name";
+           String lastName = "Puno";
+           String surname = "Last Name";
+           System.out.println( name +"  " + firstName);
+           System.out.println(surname + "   " + lastName);
+  
+    }
+    
+     public void edad(){
+        String ba = "Age";
+        System.out.println(ba + "        " + a);
+    }
+    
+    
+    public static void temp (){
+        double t = 39.2;
+        String cel = "Temperrature";
+          System.out.println(cel +" " + t);
+    }
+        
+   
+}
